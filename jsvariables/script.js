d597227b7c3d592d51f1ff25ed07d8619c117cf5
nyshada-v <nyshada.vuddanti@gmail.com>
2025-05-27 17:12:34 +0530

    commiting jsvariables

---
+++ jsvariables/script.js
@@ -0,0 +1,31 @@
+
+//let keyword
+let a;
+//checking datatype using typeof
+console.log("value of a is: ",a," and of type, ",typeof a);
+a=23;
+console.log("value of a is: ",a," and of type, ",typeof a);
+a="hello";
+console.log("value of a is: ",a," and of type, ",typeof a);
+
+//const keyword
+const x=123;
+console.log("value of a is: ",a," and of type, ",typeof a);
+
+//checking datatype of a value
+console.log("value of x is: ",x," and of type, ",typeof x);
+
+//array
+let skills=["html","css","js"];
+
+//object
+let person={
+    name:"John",
+    age:30,
+    city:"New York"
+};
+
+//function
+function test(a,b){
+    //body of function
+}
