2fb6ecf24e27d5eccc0f87c1bf17cf4e2e508fae
nyshada-v <nyshada.vuddanti@gmail.com>
2025-05-27 17:47:19 +0530

    adding jsoperators

---
+++ controlstatements/script.js
@@ -0,0 +1,48 @@
+
+
+//conditional statements
+let a=10, b=14;
+//if
+if(a>b){
+    //body
+    console.log("a is greater");
+}else if(a===b){
+    //body
+    console.log("b=a");
+}else{
+    //body
+    console.log("b is greater");
+}
+
+// //if-else
+// if(condition){
+//     //body
+// }else{
+//     //body
+// }
+
+// //if-else-if
+// if(condition1){
+//     //body
+// }
+// else if(condition2){
+//     //body
+// }
+// else if(condition3){
+//     //body
+// }
+// else{
+//     //body
+// }
+//switch
+a=2
+switch(a){
+    case 1:
+        console.log("a is 1");
+        break;
+    case 2:
+        console.log("a is 2");
+        break;
+    default:
+        console.log("a is neither 1 nor 2");
+}
